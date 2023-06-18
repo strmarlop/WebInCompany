a499cd1f1c183adc7e9556fc80a976db4ce1d26b
Esther Martinez <estmartinezl@gmail.com>
2023-06-18 20:59:07 +0200

    :construct: problems with logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,10 +20,10 @@ class LoginController extends AbstractController
         ]);
     }
 
-    // #[Route('/logout', name: 'app_logout')]
-    // public function logout(): void
-    // {
-    //     // controller can be blank: it will never be called!
-    //     // throw new \Exception('Don\'t forget to activate logout in security.yaml');
-    // }
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        // throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
