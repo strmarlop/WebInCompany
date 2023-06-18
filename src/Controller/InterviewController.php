6cb2f83fb6c893fd485af1e0936724cb3dd6fd3b
Esther Martinez <estmartinezl@gmail.com>
2023-06-18 19:51:38 +0200

    :construct: user login ok

--- src/Controller/InterviewController.php
+++ src/Controller/InterviewController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 // #[Route('/interview')]
 class InterviewController extends AbstractController
 {
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -19,4 +19,11 @@ class LoginController extends AbstractController
             'error'=> $error,
         ]);
     }
+
+    // #[Route('/logout', name: 'app_logout')]
+    // public function logout(): void
+    // {
+    //     // controller can be blank: it will never be called!
+    //     // throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    // }
 }
