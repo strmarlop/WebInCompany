d03a66407af885ace7ab3ce0948fcc868b9fa0cf
Esther Martinez <estmartinezl@gmail.com>
2023-06-19 14:56:03 +0200

    changes in home routes

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,9 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/', name: 'app_login')] ///login
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,7 +12,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
