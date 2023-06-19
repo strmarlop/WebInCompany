ff4b15c62a882ceebda950ab843da5506dce8c60
Esther Martinez <estmartinezl@gmail.com>
2023-06-19 09:26:24 +0200

    no changes

--- src/Controller/InterviewController.php
+++ src/Controller/InterviewController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 // #[Route('/interview')]
 class InterviewController extends AbstractController
 {
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
 
 class RegistrationController extends AbstractController
 {
